ae6181c49bab83b9c65ad29b7257976a1750d91d
Ramiro <ramarivera@gmail.com>
2016-10-13 21:21:28 -0300

    Re up take

--- start-kafka-shell.sh
+++ start-kafka-shell.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -e HOST_IP=$1 -e ZK=$2 -i -t gibdfrcu/kafka:latest /bin/bash
+docker	run --rm -it \
+		-v /var/run/docker.sock:/var/run/docker.sock \
+		-e HOST_IP=$1 \
+		-e ZK=$2 \
+		gibdfrcu/kafka:latest /bin/bash
 
--- start-kafka.sh
+++ start-kafka.sh
@@ -3,19 +3,24 @@
 if [[ -z "$KAFKA_PORT" ]]; then
     export KAFKA_PORT=9092
 fi
+
 if [[ -z "$KAFKA_ADVERTISED_PORT" ]]; then
     export KAFKA_ADVERTISED_PORT=$(docker port `hostname` $KAFKA_PORT | sed -r "s/.*:(.*)/\1/g")
 fi
+
 if [[ -z "$KAFKA_BROKER_ID" ]]; then
     # By default auto allocate broker ID
     export KAFKA_BROKER_ID=-1
 fi
+
 if [[ -z "$KAFKA_LOG_DIRS" ]]; then
     export KAFKA_LOG_DIRS="/kafka/kafka-logs-$HOSTNAME"
 fi
+
 if [[ -z "$KAFKA_ZOOKEEPER_CONNECT" ]]; then
     export KAFKA_ZOOKEEPER_CONNECT=$(env | grep ZK.*PORT_2181_TCP= | sed -e 's|.*tcp://||' | paste -sd ,)
 fi
+
 
 if [[ -n "$KAFKA_HEAP_OPTS" ]]; then
     sed -r -i "s/(export KAFKA_HEAP_OPTS)=\"(.*)\"/\1=\"$KAFKA_HEAP_OPTS\"/g" $KAFKA_HOME/bin/kafka-server-start.sh
@@ -32,7 +37,8 @@ do
     kafka_name=`echo "$VAR" | sed -r "s/KAFKA_(.*)=.*/\1/g" | tr '[:upper:]' '[:lower:]' | tr _ .`
     env_var=`echo "$VAR" | sed -r "s/(.*)=.*/\1/g"`
     if egrep -q "(^|^#)$kafka_name=" $KAFKA_HOME/config/server.properties; then
-        sed -r -i "s@(^|^#)($kafka_name)=(.*)@\2=${!env_var}@g" $KAFKA_HOME/config/server.properties #note that no config values may contain an '@' char
+        sed -r -i "s@(^|^#)($kafka_name)=(.*)@\2=${!env_var}@g" $KAFKA_HOME/config/server.properties 
+        #note that no config values may contain an '@' char
     else
         echo "$kafka_name=${!env_var}" >> $KAFKA_HOME/config/server.properties
     fi
@@ -60,6 +66,7 @@ term_handler() {
 
 # Capture kill requests to stop properly
 trap "term_handler" SIGHUP SIGINT SIGTERM
+
 create-topics.sh & 
 $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties &
 KAFKA_PID=$!
